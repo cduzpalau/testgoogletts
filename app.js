7c8fdfdc4d8937cfd55d28f11f663ad850aac0fb
Carles Duz Palau <3990535+cduzpalau@users.noreply.github.com>
2022-03-01 16:18:15 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,32 +1,48 @@
-const textToSpeech = require('@google-cloud/text-to-speech');
-const fs = require('fs');
-const util = require('util');
+const textToSpeech = require("@google-cloud/text-to-speech");
+const fs = require("fs");
+const util = require("util");
 
 const client = new textToSpeech.TextToSpeechClient();
 
-/**
- * TODO(developer): Uncomment the following lines before running the sample.
- */
-const text = "I would like to test how google cloud test to speech engine works";
-const outputFile = './test.mp3';
+//
+const text = process.argv[2] || "hello how are you";
+const outputFilePrefix = process.argv[3] || "Prefix";
+const languagecode = process.argv[4] || "en-GB";
+const voicename = process.argv[5] || "en-GB-Wavenet-A";
+const gender = process.argv[6] || "MALE";
+
+//Audio encoding options https://cloud.google.com/text-to-speech/docs/reference/rest/v1/text/synthesize#AudioEncoding
+const encoding = process.argv[7] || "MULAW";
+
+//Set file extension based on encoding
+const fileExtension = ".wav";
+
+if (encoding === "MP3") {
+  fileExtension = ".mp3";
+} else if (encoding == "OGG_OPUS") {
+  fileExtension = ".ogg";
+}
+
+const outputFilename = `${outputFilePrefix}-${languagecode}-${voicename}-${gender}${fileExtension}`;
 
 const request = {
-  input: {text: text},
-  voice: {languageCode: 'en-US', ssmlGender: 'FEMALE'},
-  audioConfig: {audioEncoding: 'MP3'},
+  input: { text: text },
+  voice: { languageCode: languagecode, name: voicename, sslgender: gender },
+  audioConfig: { audioEncoding: encoding },
 };
 
 (async function convertTextToSpeech() {
   try {
     const [response] = await client.synthesizeSpeech(request);
     const writeFile = util.promisify(fs.writeFile);
-    await writeFile(outputFile, response.audioContent, 'binary');
-    console.log(`Audio content written to file: ${outputFile}`);
+    await writeFile(outputFilename, response.audioContent, "binary");
+    console.log(`Audio content written to file: ${outputFilename}`);
   } catch (err) {
     console.log(err);
   }
-  client.close()
-}());
+  client.close();
+})();
+
 
 
 
